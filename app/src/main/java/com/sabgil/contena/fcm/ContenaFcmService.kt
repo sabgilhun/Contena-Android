b035f1110b76e6fdc552c1f3e15fdc77a42e032b
sabgilhun <sabgilhun@gmail.com>
2020-05-01 23:53:48 +0900

    FCM SDK 설정

---
+++ app/src/main/java/com/sabgil/contena/fcm/ContenaFcmService.kt
@@ -0,0 +1,10 @@
+package com.sabgil.contena.fcm
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class ContenaFcmService : FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+
+    }
+}
--- app/src/main/java/com/sabgil/contena/presenter/home/activity/HomeActivity.kt
+++ app/src/main/java/com/sabgil/contena/presenter/home/activity/HomeActivity.kt
@@ -1,6 +1,9 @@
 package com.sabgil.contena.presenter.home.activity
 
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 import com.sabgil.contena.R
 import com.sabgil.contena.databinding.ActivityHomeBinding
 import com.sabgil.contena.presenter.base.BaseActivity
@@ -13,6 +16,18 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
         super.onCreate(savedInstanceState)
 
         binding.bottomNavigation.setup()
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                Log.i("토큰", token ?: "없네")
+            })
     }
 
     override fun onBackPressed() = binding.bottomNavigation.goToBackTab()
