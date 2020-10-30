ec3b940d3ffa959712c9c724c7fc06609714c4a1
sabgilhun <sabgilhun@gmail.com>
2020-10-31 01:59:43 +0900

    change icon and splash image

--- app/src/main/java/com/sabgil/contena/presenter/splash/SplashActivity.kt
+++ app/src/main/java/com/sabgil/contena/presenter/splash/SplashActivity.kt
@@ -17,17 +17,18 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_spl
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         startSplashAnimation()
-        viewModel.setupObserver()
+        setupObserver()
     }
 
-    private fun SplashViewModel.setupObserver() {
-        goNext.registerObserver {
-            checkLifeCycleAndGoHome()
-        }
-        getTokenAndGoNext.registerObserver {
-            getFireBaseId { checkLifeCycleAndGoHome() }
+    private fun setupObserver() {
+        with(viewModel) {
+            goNext.registerObserver {
+                checkLifeCycleAndGoHome()
+            }
+            getTokenAndGoNext.registerObserver {
+                getFireBaseId { checkLifeCycleAndGoHome() }
+            }
         }
     }
 
@@ -47,13 +48,13 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_spl
     }
 
     private fun getFireBaseId(goNext: () -> Unit) {
-        binding.loading.visible = true
+        binding.loadingProgressBar.visible = true
         FirebaseInstanceId.getInstance().instanceId
             .addOnCompleteListener(OnCompleteListener { task ->
                 if (!task.isSuccessful) {
                     return@OnCompleteListener
                 }
-                binding.loading.visible = false
+                binding.loadingProgressBar.visible = false
                 task.result?.token?.let {
                     viewModel.saveToken(it)
                     goNext()
